4352f48f24013208c3b5e5d353f0271b0f4f3231
unknown <brenomoraes97@yahoo.com>
2023-02-10 21:55:51 -0300

    feat: created research pokemon card

---
+++ src/components/CardPokemon.vue
@@ -0,0 +1,13 @@
+<script setup>
+const pokemon = defineProps(['name', 'avatar'])
+</script>
+
+<template>
+    <main
+        class="w-40 h-44 bg-slate-200 rounded-lg flex flex-col justify-center items-center border border-zinc- cursor-pointer">
+
+        <img class="w-24 rounded-lg mb-2" :src='pokemon.avatar' alt="Pokémon">
+        <strong class=" text-cyan-900 rounded-2xl py-2 px-4 text-2xl flex justify-center">{{ pokemon.name }}</strong>
+
+    </main>
+</template>
