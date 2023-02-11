b978928ed60119cf9a94be065585368b5588b5be
unknown <brenomoraes97@yahoo.com>
2023-02-11 02:26:36 -0300

    feat: implemented function to get images of pokemons

--- src/components/CardPokemon.vue
+++ src/components/CardPokemon.vue
@@ -1,13 +1,24 @@
 <script setup>
-const pokemon = defineProps(['name', 'avatar'])
+import { onMounted, ref } from 'vue';
+import api from '../services/api';
+
+
+const pokemonProps = defineProps(['name', 'urlImgPokemon']);
+const pokemonData = ref({})
+onMounted(async () => {
+    const { data } = await api.get(`/pokemon/${pokemonProps.name}`);
+    pokemonData.value = data;
+})
 </script>
 
 <template>
     <main
         class="w-56 h-60 bg-slate-200 rounded-lg flex flex-col justify-center items-center border border-zinc- cursor-pointer">
 
-        <img class="w-40 rounded-lg mb-2" :src='pokemon.avatar' alt="Pokémon">
-        <strong class=" text-cyan-900 rounded-2xl py-2 px-4 text-2xl flex justify-center">{{ pokemon.name }}</strong>
+        <img class="w-40 rounded-lg mb-2" :src="pokemonProps.urlImgPokemon" alt="Pokémon">
+        <strong class=" text-cyan-900 rounded-2xl py-2 px-4 text-2xl flex justify-center">{{
+            pokemonProps.name
+        }}</strong>
 
     </main>
 </template>
