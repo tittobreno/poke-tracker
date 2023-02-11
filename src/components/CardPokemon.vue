d699199f7790397f24a22f719e30229cc440b3e6
unknown <brenomoraes97@yahoo.com>
2023-02-11 02:44:33 -0300

    feat: function to capitalize the first letter

--- src/components/CardPokemon.vue
+++ src/components/CardPokemon.vue
@@ -2,22 +2,26 @@
 import { onMounted, ref } from 'vue';
 import api from '../services/api';
 
-
 const pokemonProps = defineProps(['name', 'urlImgPokemon']);
 const pokemonData = ref({})
 onMounted(async () => {
     const { data } = await api.get(`/pokemon/${pokemonProps.name}`);
     pokemonData.value = data;
 })
+
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 </script>
 
 <template>
     <main
-        class="w-56 h-60 bg-slate-200 rounded-lg flex flex-col justify-center items-center border border-zinc- cursor-pointer">
+        class="w-72 h-80 bg-slate-200 rounded-lg flex flex-col justify-center items-center border border-zinc- cursor-pointer">
 
-        <img class="w-40 rounded-lg mb-2" :src="pokemonProps.urlImgPokemon" alt="Pokémon">
+        <img class="w-44 rounded-lg mb-2" :src="pokemonProps.urlImgPokemon" alt="Pokémon">
         <strong class=" text-cyan-900 rounded-2xl py-2 px-4 text-2xl flex justify-center">{{
-            pokemonProps.name
+            capitalizeFirstLetter(pokemonProps.name)
         }}</strong>
 
     </main>
