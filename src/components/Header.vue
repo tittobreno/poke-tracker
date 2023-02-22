5e471ec13c2cceaf7f3074a4d3277d605dd1918c
unknown <brenomoraes97@yahoo.com>
2023-02-22 18:30:49 -0300

    refactor: change title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
     <header class="flex items-center justify-between p-7 w-screen h-16 bg-slate-200 text-slate-900 font-bold">
         <div class='flex items-center gap-2'>
             <img class="w-10" src='../assets/pokeball.png' alt="Pokeball">
-            <strong class="text-base">Poke Bestiary</strong>
+            <strong class="text-base">Poke Tracker</strong>
         </div>
         <nav>
             <ul>
